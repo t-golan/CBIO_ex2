0d2a4a4fc84695a98123a4879523795e924366e9
tamir.golan <tamir.golan@mail.huji.ac.il>
2021-11-18 12:46:39 +0200

    shtok

--- motif_find.py
+++ motif_find.py
@@ -1,9 +1,5 @@
 import argparse
 
-
-
-
-
 def print_result(hmm, seq):
     # prints result strings in chunks of 50 chars
     for i in range(0, len(seq), 50):
@@ -14,11 +10,16 @@ def print_result(hmm, seq):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--alg', help='Algorithm (e.g. viterbi)', required=True)
-    parser.add_argument('seq', help='A sequence over the alphabet [A,C,G,T] (e.g. ACTGGACTACGTCATGCA)')
-    parser.add_argument('initial_emission', help='Path to emission table (e.g. initial_emission.tsv)')
-    parser.add_argument('p', help='transition probability p (e.g. 0.01)', type=float)
-    parser.add_argument('q', help='transition probability q (e.g. 0.5)', type=float)
+    parser.add_argument('--alg', help='Algorithm (e.g. viterbi)',
+                        required=True)
+    parser.add_argument('seq',
+                        help='A sequence over the alphabet [A,C,G,T] (e.g. ACTGGACTACGTCATGCA)')
+    parser.add_argument('initial_emission',
+                        help='Path to emission table (e.g. initial_emission.tsv)')
+    parser.add_argument('p', help='transition probability p (e.g. 0.01)',
+                        type=float)
+    parser.add_argument('q', help='transition probability q (e.g. 0.5)',
+                        type=float)
     args = parser.parse_args()
 
     if args.alg == 'viterbi':
